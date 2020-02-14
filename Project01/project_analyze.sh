a30f54cb5d0c971260efc15650a8c64169851d15
Ayed Naber <nabera@mcmaster.ca>
2020-02-14 22:17:09 +0000

    Submitting Project 01 Part 1

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "Enter the number of the feature you want to execute: "
-read fnum
+read num
 
-if [ "$fnum" -eq 2 ] ; then
+if [ "$num" -eq 2 ] ; then
         ls -lS -R ../../CS1XA3* | grep -v '^d' > filesize.txt
 	cut -b 28-31,45- filesize.txt
 fi
 
-if [ "$fnum" -eq 3 ] ; then
+if [ "$num" -eq 3 ] ; then
         git log -i --grep=merge > file1.txt
         cut -c 8-48 file1.txt > file2.txt
         commit_number=$(head -n 1 file2.txt)
         git checkout "$commit_number"
 fi
 
-if [ "$fnum" -eq 4 ] ; then
+if [ "$num" -eq 4 ] ; then
 
 	echo "Enter an extension to find how many files exist with that extension: "
 	read ext_num
